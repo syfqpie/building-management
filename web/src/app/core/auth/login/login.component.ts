a947f721bbc13490fa81f3eb03207bb8fd3af7b8
Syafiq Basri <syafiq@talentx.ai>
2022-01-27 16:00:43 +0800

    Configured login page

--- web/src/app/core/auth/login/login.component.ts
+++ web/src/app/core/auth/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+
+  // Data
+
+  // Form
+
+  // Subscriber
+  subscription: Subscription
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+    }
   }
 
 }
