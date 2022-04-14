c305f402d0b3823871e995ee018bb7686f0e93da
Syafiq Basri <syafiq@talentx.ai>
2022-04-14 22:28:36 +0800

    Initial installation

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,99 +1,21 @@
-from django.conf import settings
-from django.conf.urls import include, url
+"""core URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
 
-from datetime import datetime, timedelta
-
-from rest_framework import routers
-from rest_framework_extensions.routers import NestedRouterMixin
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
-
-from users.views import (
-    MyTokenObtainPairView,
-)
-
-class NestedDefaultRouter(NestedRouterMixin, routers.DefaultRouter):
-    pass
-
-router = NestedDefaultRouter()
-
-# Billings
-from billings.views import (
-    BillingViewSet,
-    MaintenanceBaseFeeViewSet
-)
-billings_router = router.register(
-    'billings', BillingViewSet
-)
-maintenance_base_fees_router = router.register(
-    'maintenance-base-fees', MaintenanceBaseFeeViewSet
-)
-
-# Complaints
-from complaints.views import (
-    ComplaintViewSet
-)
-complaints_router = router.register(
-    'complaints', ComplaintViewSet
-)
-
-# Medias
-from medias.views import (
-    MediaViewSet
-)
-medias_router = router.register(
-    'medias', MediaViewSet
-)
-
-# Proprietors
-from proprietors.views import (
-    ProprietorViewSet
-)
-proprietors_router = router.register(
-    'proprietors', ProprietorViewSet
-)
-
-# Units
-from units.views import (
-    BlockViewSet,
-    FloorViewSet,
-    UnitNumberViewSet,
-    UnitViewSet
-)
-blocks_router = router.register(
-    'blocks', BlockViewSet
-)
-floors_router = router.register(
-    'floors', FloorViewSet
-)
-unit_numbers_router = router.register(
-    'unit-numbers', UnitNumberViewSet
-)
-units_router = router.register(
-    'units', UnitViewSet
-)
-
-# Users
-from users.views import (
-    CustomUserViewSet
-)
-users_router = router.register(
-    'users', CustomUserViewSet
-)
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'v1/', include(router.urls)),
-    url(r'auth/', include('dj_rest_auth.urls')),
-    url(r'auth/registration', include('dj_rest_auth.registration.urls')),
-    url('auth/obtain/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    url('auth/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
