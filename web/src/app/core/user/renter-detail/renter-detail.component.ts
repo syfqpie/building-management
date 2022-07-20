0f704d7960e47e435a3ac4ba3ac2607be482b887
Syafiq Basri <syafiq@talentx.ai>
2022-07-20 14:11:37 +0800

    Added activate and deactivate feature

--- web/src/app/core/user/renter-detail/renter-detail.component.ts
+++ web/src/app/core/user/renter-detail/renter-detail.component.ts
@@ -22,6 +22,8 @@ export class RenterDetailComponent implements OnInit, OnDestroy {
 
   // Subscription
   subscription: Subscription | undefined
+  activateSubscription: Subscription | undefined
+  deactivateSubscription: Subscription | undefined
   routeSubscription: Subscription | undefined
 
   constructor(
@@ -52,6 +54,8 @@ export class RenterDetailComponent implements OnInit, OnDestroy {
   }
 
   getData(id: number) {
+    this.loadingBar.useRef('http').start()
+    this.isProcessing = true
     this.subscription = this.renterSvc.getOne(id).subscribe({
       next: () => {
         this.loadingBar.useRef('http').complete()
@@ -66,4 +70,41 @@ export class RenterDetailComponent implements OnInit, OnDestroy {
       }
     })
   }
+
+  activate() {
+    this.loadingBar.useRef('http').start()
+    this.isProcessing = true
+    this.activateSubscription = this.renterSvc.activate(this.currentRenter?.id!).subscribe({
+      next: () => {
+        this.loadingBar.useRef('http').complete()
+        this.isProcessing = false
+      },
+      error: () => {
+        this.loadingBar.useRef('http').stop()
+        this.isProcessing = false
+      },
+      complete: () => {
+        this.currentRenter = this.renterSvc.renter
+      }
+    })
+  }
+
+  deactivate() {
+    this.loadingBar.useRef('http').start()
+    this.isProcessing = true
+    this.deactivateSubscription = this.renterSvc.deactivate(this.currentRenter?.id!).subscribe({
+      next: () => {
+        this.loadingBar.useRef('http').complete()
+        this.isProcessing = false
+      },
+      error: () => {
+        this.loadingBar.useRef('http').stop()
+        this.isProcessing = false
+      },
+      complete: () => {
+        this.currentRenter = this.renterSvc.renter
+      }
+    })
+  }
+
 }
