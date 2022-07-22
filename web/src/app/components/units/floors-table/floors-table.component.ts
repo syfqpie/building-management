734278c932c9f54ab39c4d8c2b071e43f7ebaa71
Syafiq Basri <syafiq@talentx.ai>
2022-07-22 21:32:48 +0800

    Redesigned component; Added addFloor and updateFloor feature

--- web/src/app/components/units/floors-table/floors-table.component.ts
+++ web/src/app/components/units/floors-table/floors-table.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { LoadingBarService } from '@ngx-loading-bar/core';
-import { ColumnMode } from '@swimlane/ngx-datatable';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
+
+import { ColumnMode } from '@swimlane/ngx-datatable';
+import { LoadingBarService } from '@ngx-loading-bar/core';
 
 import { Floor } from 'src/app/shared/services/floors/floors.model';
 import { FloorsService } from 'src/app/shared/services/floors/floors.service';
+import { NotifyService } from 'src/app/shared/handlers/notify/notify.service';
 
 @Component({
   selector: 'app-floors-table',
@@ -16,7 +18,23 @@ export class FloorsTableComponent implements OnInit, OnDestroy {
 
   // Data
   floors: Floor[] = []
+  selectedFloor: Floor | undefined
 
+  // Form
+  addForm: FormGroup = new FormGroup({
+    floor: new FormControl(null)
+  })
+  updateForm: FormGroup = new FormGroup({
+    floor: new FormControl(null),
+    isActive: new FormControl(null)
+  })
+  formMessages = {
+    floor: [
+      { type: 'required', message: 'This field is required' }
+    ]
+  }
+
+  // Table
   tableRows: Floor[] = []
   tableLoadingIndicator: boolean = true
   tableReorderable: boolean = true
@@ -35,27 +53,36 @@ export class FloorsTableComponent implements OnInit, OnDestroy {
 
   // Checker
   isProcessing: boolean = false
-  isRegisterNew: boolean = false
+  isAddModalOpen: boolean = false
+  isUpdateModalOpen: boolean = false
   
   // Subscription
   subscription: Subscription | undefined
-
-  // Event
-  // @ViewChild(RenterRegistrationComponent) registerModal: RenterRegistrationComponent | undefined
+  addSubscription: Subscription  | undefined
+  updateSubscription: Subscription  | undefined
 
   constructor(
+    private fb: FormBuilder,
     private loadingBar: LoadingBarService,
-    private router: Router,
+    private notifySvc: NotifyService,
     private floorSvc: FloorsService
   ) { }
 
   ngOnInit(): void {
+    this.initForm()
     this.getData()
   }
 
   ngOnDestroy(): void {
+    // Unsubscribe subscriptions
     if (this.subscription) {
       this.subscription.unsubscribe()
+    }
+    if (this.addSubscription) {
+      this.addSubscription.unsubscribe()
+    }
+    if (this.updateSubscription) {
+      this.updateSubscription.unsubscribe()
     }
   }
 
@@ -78,13 +105,102 @@ export class FloorsTableComponent implements OnInit, OnDestroy {
     })
   }
 
-  // Table on select row
-  onSelect(selected: number) {
-    this.router.navigate(['management/floors/detail', selected])
+  initForm() {
+    this.addForm = this.fb.group({
+      floor: new FormControl(null, Validators.compose([
+        Validators.required
+      ]))
+    })
+    this.updateForm = this.fb.group({
+      floor: new FormControl(null, Validators.compose([
+        Validators.required
+      ])),
+      isActive: new FormControl(null, Validators.compose([
+        Validators.required
+      ]))
+    })
   }
 
-  toggleModal() {
-    // this.registerModal?.toggleModal()
+  // Table on select row
+  onSelect(selected: Floor) {
+    this.selectedFloor = selected
+    this.updateForm.controls['floor'].setValue(this.selectedFloor.floor)
+    this.updateForm.controls['isActive'].setValue(this.selectedFloor.isActive)
+    this.toggleUpdateModal()
+  }
+
+  addFloor() {
+    this.loadingBar.useRef('http').start()
+    this.isProcessing = true
+    this.addSubscription = this.floorSvc.create(
+      this.addForm.value
+    ).subscribe({
+      next: () => {
+        this.loadingBar.useRef('http').complete()
+        this.isProcessing = false
+        this.notifySvc.success(
+          'Success', 
+          'New floor has been added'
+        )
+      },
+      error: () => {
+        this.loadingBar.useRef('http').stop()
+        this.isProcessing = false
+      },
+      complete: () => {
+        // Toggle and reset
+        this.toggleAddModal()
+        this.addForm.reset()
+        this.initForm()
+
+        // Update table
+        this.getData()
+      }
+    })
+  }
+
+  patchFloor() {
+    this.loadingBar.useRef('http').start()
+    this.isProcessing = true
+    this.addSubscription = this.floorSvc.patch(
+      this.selectedFloor?.id!,
+      this.updateForm.value
+    ).subscribe({
+      next: () => {
+        this.loadingBar.useRef('http').complete()
+        this.isProcessing = false
+        this.notifySvc.success(
+          'Success', 
+          'Floor has been updated'
+        )
+      },
+      error: () => {
+        this.loadingBar.useRef('http').stop()
+        this.isProcessing = false
+      },
+      complete: () => {
+        // Toggle and reset
+        this.toggleUpdateModal()
+
+        // Update table
+        this.getData()
+      }
+    })
+  }
+
+  toggleAddModal() {
+    return this.isAddModalOpen = !this.isAddModalOpen
+  }
+
+  toggleUpdateModal() {
+    this.isUpdateModalOpen = !this.isUpdateModalOpen
+
+    // Reset
+    if (this.isUpdateModalOpen === false) {
+      this.selectedFloor= undefined
+      this.updateForm.reset()
+      this.initForm()
+    }
   }
 
 }
