f3f79007543f3661cca9d5521d0ab46d30b26659
Syafiq Basri <syafiq@talentx.ai>
2022-07-24 18:57:42 +0800

    Initial setup for Units Management page

--- web/src/app/core/user/units/units.component.ts
+++ web/src/app/core/user/units/units.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingBarService } from '@ngx-loading-bar/core';
+import { ColumnMode } from '@swimlane/ngx-datatable';
+import { Subscription } from 'rxjs';
+
+import { Unit } from 'src/app/shared/services/units/units.model';
+import { UnitsService } from 'src/app/shared/services/units/units.service';
 
 @Component({
   selector: 'app-units',
   templateUrl: './units.component.html',
   styleUrls: ['./units.component.scss']
 })
-export class UnitsComponent implements OnInit {
+export class UnitsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  // Data
+  units: Unit[] = []
+
+  tableRows: Unit[] = []
+  tableLoadingIndicator: boolean = true
+  tableReorderable: boolean = true
+  ColumnMode = ColumnMode
+  tableMessages = {
+    totalMessage: 'total of units'
+  }
+  tableClass = {
+    sortAscending: 'fa-solid fa-angle-up ms-1 small',
+    sortDescending: 'fa-solid fa-angle-down ms-1 small',
+    pagerLeftArrow: 'fa-solid fa-angle-left ms-1 small',
+    pagerRightArrow: 'fa-solid fa-angle-right ms-1 small',
+    pagerPrevious: 'fa-solid fa-angles-left ms-1 small',
+    pagerNext: 'fa-solid fa-angles-right ms-1 small'
+  }
+
+  // Checker
+  isProcessing: boolean = false
+  isRegisterNew: boolean = false
+  
+  // Subscription
+  subscription: Subscription | undefined
+
+  constructor(
+    private loadingBar: LoadingBarService,
+    private router: Router,
+    private unitSvc: UnitsService
+  ) { }
 
   ngOnInit(): void {
+    this.getData()
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+    }
+  }
+
+  getData() {
+    this.loadingBar.useRef('http').start()
+    this.isProcessing = true
+    this.subscription = this.unitSvc.getAll().subscribe({
+      next: () => {
+        this.loadingBar.useRef('http').complete()
+        this.isProcessing = false
+      },
+      error: () => {
+        this.loadingBar.useRef('http').stop()
+        this.isProcessing = false
+      },
+      complete: () => {
+        this.units = this.unitSvc.units
+        this.tableRows = [...this.units]
+      }
+    })
+  }
+
+  // Table on select row
+  onSelect(selected: number) {
+    this.router.navigate(['management/units/detail', selected])
+  }
+
+  toggleModal() {
+    // this.registerModal?.toggleModal()
   }
 
 }
