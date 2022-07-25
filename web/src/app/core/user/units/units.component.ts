dd1ce59bc54fa2661b6ebb74ab980a389f04266f
Syafiq Basri <syafiq@talentx.ai>
2022-07-25 12:25:04 +0800

    Added UnitAddComponent; Updated getData to include blockSvc, floorSvc, unitNumberSvc

--- web/src/app/core/user/units/units.component.ts
+++ web/src/app/core/user/units/units.component.ts
@@ -2,7 +2,14 @@ import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import { ColumnMode } from '@swimlane/ngx-datatable';
-import { Subscription } from 'rxjs';
+import { forkJoin, Subscription } from 'rxjs';
+import { UnitAddComponent } from 'src/app/components/units/unit-add/unit-add.component';
+import { Block } from 'src/app/shared/services/blocks/blocks.model';
+import { BlocksService } from 'src/app/shared/services/blocks/blocks.service';
+import { Floor } from 'src/app/shared/services/floors/floors.model';
+import { FloorsService } from 'src/app/shared/services/floors/floors.service';
+import { UnitNumber } from 'src/app/shared/services/unit-numbers/unit-numbers.model';
+import { UnitNumbersService } from 'src/app/shared/services/unit-numbers/unit-numbers.service';
 
 import { Unit } from 'src/app/shared/services/units/units.model';
 import { UnitsService } from 'src/app/shared/services/units/units.service';
@@ -16,6 +23,9 @@ export class UnitsComponent implements OnInit, OnDestroy {
 
   // Data
   units: Unit[] = []
+  blocks: Block[] = []
+  floors: Floor[] = []
+  unitNumbers: UnitNumber[] = []
 
   tableRows: Unit[] = []
   tableLoadingIndicator: boolean = true
@@ -40,10 +50,15 @@ export class UnitsComponent implements OnInit, OnDestroy {
   // Subscription
   subscription: Subscription | undefined
 
+  @ViewChild(UnitAddComponent) unitAdd: UnitAddComponent | undefined
+
   constructor(
     private loadingBar: LoadingBarService,
     private router: Router,
-    private unitSvc: UnitsService
+    private unitSvc: UnitsService,
+    private blockSvc: BlocksService,
+    private floorSvc: FloorsService,
+    private unitNumberSvc: UnitNumbersService
   ) { }
 
   ngOnInit(): void {
@@ -59,7 +74,12 @@ export class UnitsComponent implements OnInit, OnDestroy {
   getData() {
     this.loadingBar.useRef('http').start()
     this.isProcessing = true
-    this.subscription = this.unitSvc.getAll().subscribe({
+    this.subscription = forkJoin([
+      this.unitSvc.getAll(),
+      this.blockSvc.getAll(),
+      this.floorSvc.getAll(),
+      this.unitNumberSvc.getAll()
+    ]).subscribe({
       next: () => {
         this.loadingBar.useRef('http').complete()
         this.isProcessing = false
@@ -70,6 +90,9 @@ export class UnitsComponent implements OnInit, OnDestroy {
       },
       complete: () => {
         this.units = this.unitSvc.units
+        this.blocks = this.blockSvc.blocks
+        this.floors = this.floorSvc.floors
+        this.unitNumbers = this.unitNumberSvc.unitNumbers
         this.tableRows = [...this.units]
       }
     })
@@ -81,7 +104,7 @@ export class UnitsComponent implements OnInit, OnDestroy {
   }
 
   toggleModal() {
-    // this.registerModal?.toggleModal()
+    this.unitAdd?.toggleModal()
   }
 
 }
