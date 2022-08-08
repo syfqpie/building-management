51f731b7d1c01151024b57a6c9dbc6bf81f2369c
Syafiq Basri <syafiq@talentx.ai>
2022-08-08 13:28:39 +0800

    Added docstring

--- web/src/app/shared/services/helpers/helpers.service.ts
+++ web/src/app/shared/services/helpers/helpers.service.ts
@@ -7,8 +7,13 @@ export class HelpersService {
 
   constructor() { }
 
+  /**
+   * Get a new object containing only the changed field values
+   */
   getFormChangedValuesOnly(source: any, usedForm: any) {
     let newForm: any = {}
+
+    // Iterate to get changed values
     Object.keys(usedForm).forEach(
       (key) => {
         if (source[key] !== usedForm[key]) {
