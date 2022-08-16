16f862fc3f6796830e693d277c8e8ab9a77717b7
Syafiq Basri <syafiq@talentx.ai>
2022-08-16 14:32:22 +0800

    Added feature to add unit related and assignee; Minor change on design

--- web/src/app/components/tickets/add-ticket/add-ticket.component.ts
+++ web/src/app/components/tickets/add-ticket/add-ticket.component.ts
@@ -3,12 +3,16 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
-import { LoadingBarService } from '@ngx-loading-bar/core';
-
 import { collapsedAnimation } from 'src/app/shared/animations/animation';
-import { TicketsService } from 'src/app/shared/services/tickets/tickets.service';
+import { LoadingBarService } from '@ngx-loading-bar/core';
 import { NotifyService } from 'src/app/shared/handlers/notify/notify.service';
+
 import { TicketCategory, TicketPriority } from 'src/app/shared/services/tickets/tickets.model';
+import { UnitNo } from 'src/app/shared/services/units/units.model';
+import { UserEmail } from 'src/app/shared/services/users/users.model';
+import { TicketsService } from 'src/app/shared/services/tickets/tickets.service';
+import { UnitsService } from 'src/app/shared/services/units/units.service';
+import { UsersService } from 'src/app/shared/services/users/users.service';
 
 @Component({
   selector: 'app-add-ticket',
@@ -19,6 +23,10 @@ import { TicketCategory, TicketPriority } from 'src/app/shared/services/tickets/
   ]
 })
 export class AddTicketComponent implements OnInit, OnDestroy {
+
+  // Data
+  units: UnitNo[] = []
+  assignees: UserEmail[] = []
 
   // Predefined
   ticketCategory = TicketCategory
@@ -52,20 +60,22 @@ export class AddTicketComponent implements OnInit, OnDestroy {
   }
 
   // Checker
-  isProcessing: boolean = false
-  isUnitRelated: boolean = false
-  isAssigning: boolean = false
-  isModalOpen: boolean = false
+  isProcessing: boolean = false // http req
+  isUnitRelated: boolean = false // is form unit related
+  isAssigning: boolean = false // is form assigning assignee
+  isFetchingOpts: boolean = false // opts fetching
 
   // Subscription
-  subscription: Subscription | undefined
+  subscription: Subscription = new Subscription
 
   constructor(
     private fb: FormBuilder,
     private loadingBar: LoadingBarService,
     private router: Router,
     private notifySvc: NotifyService,
-    private ticketSvc: TicketsService
+    private ticketSvc: TicketsService,
+    private unitSvc: UnitsService,
+    private userSvc: UsersService
   ) { }
 
   ngOnInit(): void {
@@ -104,7 +114,8 @@ export class AddTicketComponent implements OnInit, OnDestroy {
   addTicket() {
     this.loadingBar.useRef('http').start()
     this.isProcessing = true
-    this.subscription = this.ticketSvc.create(this.addForm.value).subscribe({
+
+    this.subscription.add(this.ticketSvc.create(this.addForm.value).subscribe({
       next: () => {
         this.loadingBar.useRef('http').complete()
         this.isProcessing = false
@@ -118,12 +129,90 @@ export class AddTicketComponent implements OnInit, OnDestroy {
         this.isProcessing = false
       },
       complete: () => {
+        // Reset and re-init form
         this.addForm.reset()
         this.initForm()
-
+        
+        // Navigate to new ticket
         this.router.navigate(['/management/tickets/detail', this.ticketSvc.ticket?.id])
       }
-    })
+    }))
+  }
+
+  toggleAssigning() {
+    this.isAssigning = !this.isAssigning
+
+    if (this.isAssigning) {
+      // Enable field
+      this.addForm.get('assignee')?.enable()
+
+      // Get data if choice = 0
+      if (this.assignees.length === 0) {
+        this.getAssignees()
+      }
+    } else {
+      // Reset field
+      this.addForm.get('assignee')?.patchValue(null)
+      // Disable field
+      this.addForm.get('assignee')?.disable()
+    }
+  }
+
+  toggleUnitRelated() {
+    this.isUnitRelated = !this.isUnitRelated
+
+    if (this.isUnitRelated) {
+      // Enable field
+      this.addForm.get('unit')?.enable()
+
+      // Get data if choice = 0
+      if (this.units.length === 0) {
+        this.getUnits()
+      }
+    } else {
+      // Reset field
+      this.addForm.get('unit')?.patchValue(null)
+      // Disable field
+      this.addForm.get('unit')?.disable()
+    }
+  }
+
+  getAssignees() {
+    this.loadingBar.useRef('http').start()
+    this.isFetchingOpts = true
+
+    this.subscription.add(this.userSvc.filterSimplified('user_type=1').subscribe({
+      next: () => {
+        this.loadingBar.useRef('http').complete()
+        this.isFetchingOpts = false
+      },
+      error: () => {
+        this.loadingBar.useRef('http').stop()
+        this.isFetchingOpts = false
+      },
+      complete: () => {
+        this.assignees = this.userSvc.usersChoice
+      }
+    }))
+  }
+
+  getUnits() {
+    this.loadingBar.useRef('http').start()
+    this.isFetchingOpts = true
+    
+    this.subscription.add(this.unitSvc.getAll().subscribe({
+      next: () => {
+        this.loadingBar.useRef('http').complete()
+        this.isFetchingOpts = false
+      },
+      error: () => {
+        this.loadingBar.useRef('http').stop()
+        this.isFetchingOpts = false
+      },
+      complete: () => {
+        this.units = this.unitSvc.units
+      }
+    }))
   }
 
 }
