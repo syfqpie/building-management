3dd66e8462bd83371e7b261483c61a3f4523cb5e
Syafiq Basri <syafiq@talentx.ai>
2022-08-24 11:39:08 +0800

    Added BlockNo, FloorNo, ParkingExtended model

--- web/src/app/shared/services/parkings/parkings.model.ts
+++ web/src/app/shared/services/parkings/parkings.model.ts
@@ -1,3 +1,7 @@
+import { BlockNo } from '../blocks/blocks.model';
+import { FloorNo } from '../floors/floors.model';
+import { Resident } from '../residents/residents.model';
+
 export class Parking {
     constructor(
         public id: number,
@@ -5,6 +9,21 @@ export class Parking {
         public block: number,
         public floor: number,
         public owner: number,
+        public isActive: boolean,
+        public createdAt: string,
+        public createdBy: number,
+        public lastModifiedAt: string,
+        public lastModifiedBy: number
+    ) {}
+}
+
+export class ParkingExtended {
+    constructor(
+        public id: number,
+        public lotNo: string,
+        public block: BlockNo,
+        public floor: FloorNo,
+        public owner: Resident,
         public isActive: boolean,
         public createdAt: string,
         public createdBy: number,
