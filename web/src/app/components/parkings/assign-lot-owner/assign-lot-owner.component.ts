bb3e4011c30c61b53df13bb5d16673e5fdb5cb00
Syafiq Basri <syafiq@talentx.ai>
2022-08-27 21:04:31 +0800

    Redesigned AssignLotOwner

--- web/src/app/components/parkings/assign-lot-owner/assign-lot-owner.component.ts
+++ web/src/app/components/parkings/assign-lot-owner/assign-lot-owner.component.ts
@@ -1,35 +1,26 @@
-import { 
-  Component,
-  EventEmitter,
-  Input,
-  OnDestroy,
-  OnInit,
-  Output } from '@angular/core';
-import {
-  catchError,
-  concat,
-  debounceTime,
-  distinctUntilChanged,
-  filter,
-  Observable,
-  of,
-  Subject,
-  Subscription, 
-  switchMap, 
-  tap } from 'rxjs';
+import { Component, EventEmitter, Input,
+  OnDestroy, OnInit, Output } from '@angular/core';
+import { catchError, concat, debounceTime, distinctUntilChanged, filter,
+  Observable, of, Subject, Subscription,  switchMap,  tap } from 'rxjs';
+  import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { collapsedAnimation } from 'src/app/shared/animations/animation';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import { NotifyService } from 'src/app/shared/handlers/notify/notify.service';
 
 import { Resident } from 'src/app/shared/services/residents/residents.model';
-import { ResidentsService } from 'src/app/shared/services/residents/residents.service';
-import { UnitsService } from 'src/app/shared/services/units/units.service';
+import { Vehicle, VehicleType } from 'src/app/shared/services/vehicles/vehicles.model';
 import { ParkingsService } from 'src/app/shared/services/parkings/parkings.service';
+import { ResidentsService } from 'src/app/shared/services/residents/residents.service';
+import { VehiclesService } from 'src/app/shared/services/vehicles/vehicles.service';
 
 @Component({
   selector: 'app-assign-lot-owner',
   templateUrl: './assign-lot-owner.component.html',
-  styleUrls: ['./assign-lot-owner.component.scss']
+  styleUrls: ['./assign-lot-owner.component.scss'],
+  animations: [
+    collapsedAnimation
+  ]
 })
 export class AssignLotOwnerComponent implements OnInit, OnDestroy {
 
@@ -40,34 +31,86 @@ export class AssignLotOwnerComponent implements OnInit, OnDestroy {
   residents$?: Observable<Resident[]>
   residentInput$: Subject<string> = new Subject<string>()
   selectedResident: Resident | undefined
+  vehicles: Vehicle[] = []
+  selectedVehicle: Vehicle | undefined
+  vehicleType = VehicleType
+  currentStep: number = 0
+
+  // Form
+  assignForm: FormGroup = new FormGroup({
+    resident: new FormControl(null),
+    vehicle: new FormControl(null)
+  })
+  addForm: FormGroup = new FormGroup({
+    plateNo: new FormControl(null),
+    vehicleType: new FormControl(null),
+    resident: new FormControl(null)
+  })
+  formMessages = {
+    plateNo: [
+      { type: 'required', message: 'Field is required' }
+    ],
+    vehicleType: [
+      { type: 'required', message: 'Field is required' }
+    ]
+  }
 
   // Checker
   isProcessing: boolean = false
-  isAssigning: boolean = false
   isSearching: boolean = false
+  isAddVehicle: boolean = false
 
   // Subscription
-  svcSubscription: Subscription = new Subscription
+  subscription: Subscription = new Subscription
   
   // Event
   @Output() changedEvent: EventEmitter<boolean> = new EventEmitter()
 
   constructor(
+    private fb: FormBuilder,
     private loadingBar: LoadingBarService,
     private notifySvc: NotifyService,
     private parkingSvc: ParkingsService,
-    private residentSvc: ResidentsService
+    private residentSvc: ResidentsService,
+    private vehicleSvc: VehiclesService
   ) { }
 
   ngOnInit(): void {
     this.searchResident()
+    this.initAssignForm()
+    this.initAddForm()
   }
 
   ngOnDestroy(): void {
-    // Unsubscribe services subscription
-    if (this.svcSubscription) {
-      this.svcSubscription.unsubscribe()
+    // Unsubscribe
+    if (this.subscription) {
+      this.subscription.unsubscribe()
     }
+  }
+
+  initAssignForm() {
+    this.assignForm = this.fb.group({
+      resident: new FormControl(null, Validators.compose([
+        Validators.required
+      ])),
+      vehicle: new FormControl(null, Validators.compose([
+        Validators.required
+      ]))
+    })
+  }
+
+  initAddForm() {
+    this.addForm = this.fb.group({
+      plateNo: new FormControl(null, Validators.compose([
+        Validators.required
+      ])),
+      vehicleType: new FormControl(null, Validators.compose([
+        Validators.required
+      ])),
+      resident: new FormControl(null, Validators.compose([
+        Validators.required
+      ]))
+    })
   }
 
   searchResident() {
@@ -90,17 +133,90 @@ export class AssignLotOwnerComponent implements OnInit, OnDestroy {
     )
   }
 
+  onSelectResident(resident: Resident) {
+    // Assign resident to form
+    this.selectedResident = resident
+    this.assignForm.controls['resident'].setValue(this.selectedResident.id)
+    
+    // Reset vehicle field
+    this.selectedVehicle = undefined
+    this.assignForm.controls['vehicle'].setValue(null)
+  }
+
+  nextStep() {
+    this.currentStep = this.currentStep + 1
+
+    if (this.currentStep === 1) {
+      // Retrieve resident's vehicles
+      this.getVehicles()
+
+      // Reset and update form value
+      this.resetForm()
+    }
+  }
+
+  getVehicles() {
+    this.isProcessing = true
+    this.loadingBar.useRef('http').start()
+
+    this.subscription.add(
+      this.vehicleSvc.list(`resident=${this.selectedResident?.id!}`).subscribe({
+        next: () => {
+          this.isProcessing = false
+          this.loadingBar.useRef('http').complete()
+        },
+        error: () => {
+          this.isProcessing = false
+          this.loadingBar.useRef('http').stop()
+        },
+        complete: () => {
+          this.vehicles = this.vehicleSvc.vehicles
+        }
+      }))
+  }
+
+  addVehicle() {
+    this.isProcessing = true
+    this.loadingBar.useRef('http').start()
+
+    this.subscription.add(
+      this.vehicleSvc.create(this.addForm.value).subscribe({
+        next: () => {
+          this.isProcessing = false
+          this.loadingBar.useRef('http').complete()
+
+          this.notifySvc.success(
+            'Success', 
+            'Vehicle has been added to resident'
+          )
+        },
+        error: () => {
+          this.isProcessing = false
+          this.loadingBar.useRef('http').stop()
+        },
+        complete: () => {
+          // Refresh vehicle data
+          this.getVehicles()
+
+          // Reset form and disable add vehicle
+          this.resetForm()
+          this.isAddVehicle = false
+        }
+      }))
+  }
+
+  onSelectVehicle(vehicle: Vehicle) {
+    // Assign vehicle to form
+    this.selectedVehicle = vehicle
+  }
+
   assignOwner() {
     this.isProcessing = true
     this.loadingBar.useRef('http').start()
 
-    const formSetup = {
-      resident: this.selectedResident!.id
-    }
-
-    this.svcSubscription.add(this.parkingSvc.assignOwner(
+    this.subscription.add(this.parkingSvc.assignResident(
       this.parkingId,
-      formSetup
+      this.assignForm.value
     ).subscribe({
       next: () => {
         this.isProcessing = false
@@ -111,18 +227,21 @@ export class AssignLotOwnerComponent implements OnInit, OnDestroy {
           'Owner has been assigned to this parking'
         )
       },
-      error: () => {
+      error: (err) => {
         this.isProcessing = false
         this.loadingBar.useRef('http').stop()
+
+        if (err.status === 403 && 'detail' in err.error) {
+          this.notifySvc.error(
+            'Error', 
+            err.error.detail
+          )
+        }
       },
       complete: () => {
         this.changedEvent.emit(true)
       }
     }))
-  }
-
-  onSelectResident(resident: Resident) {
-    this.selectedResident = resident
   }
 
   cancelAssignOwner() {
@@ -132,6 +251,13 @@ export class AssignLotOwnerComponent implements OnInit, OnDestroy {
         this.searchResident()
       }, 500
     )
+  }
+
+  resetForm() {
+    // Reset and update form value
+    this.addForm.reset()
+    this.initAddForm()
+    this.addForm.controls['resident'].setValue(this.selectedResident?.id!)
   }
 
 }
