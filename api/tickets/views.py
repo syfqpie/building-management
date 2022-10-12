f7208be7fbcdcd9acb2991f31b8231664e2fbd08
Syafiq Basri <altsyafiq@gmail.com>
2022-10-12 21:05:53 +0800

    Added DocuConfigTicket; Configured TicketViewSet for drf-yasg documentations

--- api/tickets/views.py
+++ api/tickets/views.py
@@ -21,7 +21,7 @@ from utils.helpers import dict_snake_to_camel, camel_to_capitalize
 from users.models import UserType
 from utils.auth.permissions import IsAdminStaff, IsSuperAdmin
 
-from .docs import DocuConfigTicketTag
+from .docs import DocuConfigTicketTag, DocuConfigTicket
 from .models import (
     TicketPriority, TicketStatus, TicketTag, Ticket, TicketActivity, TicketComment
 )
@@ -61,7 +61,7 @@ class TicketTagViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         """Override perform_create to update created_by"""
-        
+
         request = serializer.context['request']
         serializer.save(created_by=request.user)
 
@@ -72,23 +72,13 @@ class TicketTagViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
         serializer.save(last_modified_by=request.user)
 
 
-@method_decorator(name='list', decorator=swagger_auto_schema(
-    tags=['Tickets'], operation_id='Get tickets',
-    operation_description='List all tickets'
-))
-@method_decorator(name='retrieve', decorator=swagger_auto_schema(
-    tags=['Tickets'], operation_id='Get ticket',
-    operation_description='Retrieve ticket information'
-))
-@method_decorator(name='create', decorator=swagger_auto_schema(
-    tags=['Tickets'], operation_id='Create a ticket',
-    operation_description='Create a new ticket'
-))
-@method_decorator(name='partial_update', decorator=swagger_auto_schema(
-    tags=['Tickets'], operation_id='Edit ticket',
-    operation_description='Edit ticket information'
-))
+@method_decorator(name='list', decorator=DocuConfigTicket.LIST)
+@method_decorator(name='retrieve', decorator=DocuConfigTicket.RETRIEVE)
+@method_decorator(name='create', decorator=DocuConfigTicket.CREATE)
+@method_decorator(name='partial_update', decorator=DocuConfigTicket.PARTIAL_UPDATE)
 class TicketViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
+    """Viewset for Ticket model"""
+
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     serializer_class_validator = {
@@ -116,16 +106,20 @@ class TicketViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
         return [permission() for permission in permission_classes]
 
     def get_serializer_class(self):
-        # Check serializer class by action
+        """Override get_serializer_class for default action"""
+
         if hasattr(self, 'serializer_class_admin'):
             return self.serializer_class_admin.get(self.action, self.serializer_class)
 
         # Return original class
         return super().get_serializer_class()
     
-    # For validation only
     def get_serializer_validator_class(self):
-        # Check serializer validation class by action
+        """
+        Override get_serializer_validator_class for
+        serializer validation class by action
+        """
+
         if hasattr(self, 'serializer_class_validator'):
             return self.serializer_class_validator.get(self.action, self.serializer_class)
 
@@ -133,19 +127,22 @@ class TicketViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
         return super().get_serializer_class()
 
     def perform_create(self, serializer):
-        # Saving user to created_by
+        """Override perform_create to update created_by"""
+
         request = serializer.context['request']
         serializer.save(created_by=request.user)
 
     def perform_update(self, serializer):
-        # Saving user to last_modified_by
+        """Override perform_update to update last_modified_by"""
+
         request = serializer.context['request']
         serializer.save(last_modified_by=request.user)
     
     def get_serializer(self, *args, **kwargs):
+        """Override get_serializer to get serializer validator"""
+
         is_partial = kwargs.get('partial', None)
 
-        # Check to get serializer validator
         if self.action == 'update_status' and not is_partial == None:
             serializer_class = self.get_serializer_validator_class()
             kwargs.setdefault('context', self.get_serializer_context())
@@ -154,21 +151,21 @@ class TicketViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
         # Return default method
         return super().get_serializer(*args, **kwargs)
     
-    # Get extended ticket
-    @swagger_auto_schema(tags=['Tickets'], operation_id='Get ticket extended',
-        operation_description='Get ticket extended information')
+    @swagger_auto_schema(**DocuConfigTicket.RETRIEVE_EXT.value)
     @action(methods=['GET'], detail=True, url_path='extended')
     def retrieve_ext(self, request, *args, **kwargs):
+        """Get extended ticket"""
+
         ticket = self.get_object()
         serializer = self.get_serializer(ticket, many=False)
         return Response(serializer.data)
     
-    # Update status
-    @swagger_auto_schema(tags=['Tickets'], operation_id='Update ticket status',
-        operation_description='Update a ticket status')
+    @swagger_auto_schema(**DocuConfigTicket.UPDATE_STATUS.value)
     @action(methods=['PATCH'], detail=True, url_path='update-status')
     def update_status(self, request, *args, **kwargs):
-        # Get ticket instance
+        """Update ticket status"""
+
+        # Instantiate ticket
         ticket = self.get_object()
 
         # Serialize and check if form is valid
@@ -207,20 +204,21 @@ class TicketViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
             headers=headers
         )
 
-    # Get ticket overview
-    @swagger_auto_schema(tags=['Tickets'], operation_id='Get ticket overview',
-        operation_description='Get ticket overview information')
+    @swagger_auto_schema(**DocuConfigTicket.OVERVIEW.value)
     @action(methods=['GET'], detail=False, url_path='overview')
     def get_overview(self, request, *args, **kwargs):
         """
-            Month over Month growth
+        Get ticket overview
 
-            x = new month
-            y = old month
-            z = difference percentage
-            z = (x - y) / y * 100
-            None = infinty
+        Month over Month growth
+
+        x = new month
+        y = old month
+        z = difference percentage
+        z = (x - y) / y * 100
+        None = infinty
         """
+        
         # Get this year's tickets
         current_date = now()
         tickets = Ticket.objects.all().filter(created_at__year=current_date.year)
@@ -278,11 +276,11 @@ class TicketViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
 
         return JsonResponse(dict_snake_to_camel(overview_total))
     
-    # Get ticket status overview
-    @swagger_auto_schema(tags=['Tickets'], operation_id='Get ticket status overview',
-        operation_description='Get ticket status overview information')
+    @swagger_auto_schema(**DocuConfigTicket.STATUS_OVERVIEW.value)
     @action(methods=['GET'], detail=False, url_path='status-overview')
     def get_status_overview(self, request, *args, **kwargs):
+        """Get ticket status overview"""
+
         # Get this year's tickets
         current_date = now()
         tickets = Ticket.objects.all().filter(created_at__year=current_date.year)
@@ -330,11 +328,11 @@ class TicketViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
         
         return JsonResponse(status_monthly, safe=False)
     
-    # Get ticket priority overview
-    @swagger_auto_schema(tags=['Tickets'], operation_id='Get ticket priority overview',
-        operation_description='Get ticket priority overview information')
+    @swagger_auto_schema(**DocuConfigTicket.PRIORITY_OVERVIEW.value)
     @action(methods=['GET'], detail=False, url_path='priority-overview')
     def get_priority_overview(self, request, *args, **kwargs):
+        """Get ticket priority overview"""
+
         # Get this year's tickets
         current_date = now()
         tickets = Ticket.objects.all().filter(created_at__year=current_date.year)
