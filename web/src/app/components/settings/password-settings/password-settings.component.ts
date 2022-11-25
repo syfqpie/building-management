0ccd3245a4bd6037a45d39d76d644e166c0a20a9
Syafiq Basri <altsyafiq@gmail.com>
2022-11-25 19:01:56 +0800

    :hammer: Refactored PasswordSetting

--- web/src/app/components/settings/password-settings/password-settings.component.ts
+++ web/src/app/components/settings/password-settings/password-settings.component.ts
@@ -3,10 +3,10 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Subscription } from 'rxjs';
 
 import { LoadingBarService } from '@ngx-loading-bar/core';
-import { NotifyService } from 'src/app/shared/handlers/notify/notify.service';
-import PasswordValidation from 'src/app/shared/handlers/validators/password-match.validator';
 
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { NotifyService } from 'src/app/shared/handlers/notify/notify.service';
+import PasswordValidation from 'src/app/shared/handlers/validators/password-match.validator';
 
 @Component({
   selector: 'app-password-settings',
@@ -36,7 +36,7 @@ export class PasswordSettingsComponent implements OnInit, OnDestroy {
   isProcessing: boolean = false
 
   // Subscriber
-  subscription: Subscription | undefined
+  subscription: Subscription = new Subscription
 
   constructor(
     private fb: FormBuilder,
@@ -56,6 +56,10 @@ export class PasswordSettingsComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Initialize form with current data
+   * and validators
+   */
   initForm() {
     this.changeForm = this.fb.group({
       newPassword1: new FormControl(null, Validators.compose([
@@ -71,38 +75,48 @@ export class PasswordSettingsComponent implements OnInit, OnDestroy {
     })
   }
 
+  /**
+   * Trigger http request to apply
+   * changes
+   */
   change() {
+    // For loading status
     this.loadingBar.useRef('http').start()
     this.isProcessing = true
 
-    this.subscription = this.authSvc.changePassword(this.changeForm.value).subscribe({
-      next: () => {
-        this.loadingBar.useRef('http').complete()
-        this.isProcessing = false
-        
-        this.notifySvc.success(
-          'Success',
-          'New password has been saved'
-        )
-      },
-      error: (err) => {
-        this.loadingBar.useRef('http').stop()
-        this.isProcessing = false
-        
-        let errorMsg = ''
-
-        if (err.status !== 0) {
-          if ('nonFieldErrors' in err.error) {
-            errorMsg = err.error.nonFieldErrors[0]
-          } else if ('detail' in err.error) {
-            errorMsg = err.error.detail
-          }
-        }
-
-        this.notifySvc.error('Error', errorMsg)
-      },
-      complete: () => { }
-    })
+    this.subscription.add(
+      this.authSvc.changePassword(this.changeForm.value)
+          .subscribe({
+            next: () => {
+              // Update loading status and show toastr
+              this.loadingBar.useRef('http').complete()
+              this.isProcessing = false
+              this.notifySvc.success(
+                'Success',
+                'New password has been saved'
+              )
+            },
+            error: (err) => {
+              // Update loading status
+              this.loadingBar.useRef('http').stop()
+              this.isProcessing = false
+              
+              let errorMsg = ''
+              
+              // Get error message from response
+              if (err.status !== 0) {
+                if ('nonFieldErrors' in err.error) {
+                  errorMsg = err.error.nonFieldErrors[0]
+                } else if ('detail' in err.error) {
+                  errorMsg = err.error.detail
+                }
+              }
+              
+              // Show toastr
+              this.notifySvc.error('Error', errorMsg)
+            },
+            complete: () => { }
+          }))
   }
 
 }
