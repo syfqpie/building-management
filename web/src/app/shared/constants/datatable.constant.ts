4cdef5c17c4d19eaa9a0d809ed9afb8a7c2a44d1
Syafiq Basri <altsyafiq@gmail.com>
2022-11-29 20:25:39 +0800

    :hammer: Added datatable constants

---
+++ web/src/app/shared/constants/datatable.constant.ts
@@ -0,0 +1,12 @@
+export const TABLE_CLASS = {
+    sortAscending: 'fa-solid fa-angle-up ms-2 small',
+    sortDescending: 'fa-solid fa-angle-down ms-2 small',
+    pagerLeftArrow: 'fa-solid fa-angle-left small',
+    pagerRightArrow: 'fa-solid fa-angle-right small',
+    pagerPrevious: 'fa-solid fa-angles-left small',
+    pagerNext: 'fa-solid fa-angles-right small'
+}
+
+export const TABLE_MESSAGES = {
+    totalMessage: 'total of records'
+}
