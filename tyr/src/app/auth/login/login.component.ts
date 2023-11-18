a23f78fb4195ebf8b0a0d5d388eceedd7203c5c1
Syafiq Basri <altsyafiq@gmail.com>
2023-11-18 23:48:01 +0800

    refactor: improved format, error handling

--- tyr/src/app/auth/login/login.component.ts
+++ tyr/src/app/auth/login/login.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { first } from 'rxjs';
+import { Subscription, first } from 'rxjs';
 
 import { LoadableComponent } from 'src/app/shared/models/base.model';
 import { AuthService } from '../services/auth.service';
+import { AuthFormMessage } from '../auth.constant';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
 	styles: []
 })
-export class LoginComponent extends LoadableComponent implements OnInit {
+export class LoginComponent extends LoadableComponent implements OnInit, OnDestroy {
 	returnUrl: string | null = null
 
 	form: FormGroup = new FormGroup({
 		username: new FormControl(null),
 		password: new FormControl(null)
 	})
+	formMessages = {
+		username: AuthFormMessage.email,
+		password: AuthFormMessage.password
+	}
+
+	subscription = new Subscription()
 
 	constructor(
 		private fb: FormBuilder,
@@ -32,39 +39,47 @@ export class LoginComponent extends LoadableComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.initForm()
-		console.log('Login initialized', this.isLoading)
+	}
+
+	ngOnDestroy(): void {
+		if (this.subscription) this.subscription.unsubscribe()
 	}
 
 	private initForm(): void {
 		this.form = this.fb.group({
-			username: new FormControl(null, Validators.compose([
-				Validators.required,
-				Validators.email
-			])),
-			password: new FormControl(null, Validators.compose([
-				Validators.required,
-				Validators.minLength(8)
-			]))
+			username: new FormControl(null, {
+				validators: [
+					Validators.required,
+					Validators.email
+				],
+				updateOn: 'blur'
+			}),
+			password: new FormControl(null, {
+				validators: [
+					Validators.required,
+					Validators.minLength(8)
+				],
+				updateOn: 'blur'
+			})
 		})
 	}
 
 	private login(): void {
-		this.isLoading = true
-		this.authSvc.login(this.form.value)
-					.pipe(first())
-					.subscribe({
-						next: () => {
-							this.isLoading = false
-						},
-						error: (err: HttpErrorResponse) => {
-							this.isLoading = false
-							console.log(err)
-						},
-						complete: () => {
-							console.log('complete')
-							// this.router.navigate(['/dashboard'])
-						}
-					})
+		this.toggleLoader()
+		this.subscription.add(this.authSvc.login(this.form.value)
+			.pipe(first())
+			.subscribe({
+				next: () => {
+					this.toggleLoader()
+				},
+				error: (err: HttpErrorResponse) => {
+					this.toggleLoader()
+					console.log(err)
+				},
+				complete: () => {
+					this.router.navigate(['/dashboard'])
+				}
+			}))
 	}
 	
 	doPreLogin(): void {
