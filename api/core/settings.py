5a8071ed66ea4bd81f34fdef3fe65eca322b2bb5
Syafiq Basri <altsyafiq@gmail.com>
2024-03-28 16:27:29 +0800

    chore: added render url

--- api/core/settings.py
+++ api/core/settings.py
@@ -235,11 +235,13 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1',
-    'http://localhost'
+    'http://localhost',
+    'building-management-qe63.onrender.com',
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
     'http://127.0.0.1',
-    'http://localhost'
+    'http://localhost',
+    'building-management-qe63.onrender.com',
 ]
 CORS_EXPOSE_HEADERS = ['Content-Disposition']
 
